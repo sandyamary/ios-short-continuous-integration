bd21a66adfe1df79421034cb4022884a97135f4f
mudumala <mary.udumala@disney.com>
2017-04-08 14:45:25 -0700

    add UI test

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -21,6 +21,7 @@ class RomanNumeralCalculatorUITests: XCTestCase {
         XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        XCUIDevice.shared().orientation = .portrait
     }
     
     override func tearDown() {
@@ -28,9 +29,16 @@ class RomanNumeralCalculatorUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAddOnePlusOne() {
+        
+        let app = XCUIApplication()
+        let calculatedValue = app.staticTexts["calculated_value"]
+        let iButton = app.buttons["I"]
+        iButton.tap()
+        XCTAssertEqual(calculatedValue.label, "I")
+        app.buttons["+"].tap()
+        iButton.tap()
+        XCTAssertEqual(calculatedValue.label, "II")
     }
     
 }
